fa226a5f98e07eec7be0e6de78caf5cf3fc1bbea
Marco Graciano <magraciano@radioformula.com.mx>
2025-03-31 20:33:27 -0600

    Se agregaron carpetas al gitignore, para evitar llevar el registro de los cambios en carpetas dinámicas.

--- backend/LLM/__init__.py
+++
@@ -1,7 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-EMBEDDINGS_MODEL = os.getenv("EMBEDDINGS_MODEL")  # Modelo para generar embeddings
-OLLAMAMODELS= os.getenv("OLLAMAMODELS")
